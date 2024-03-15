3af44c7cef3b370aeae3ea8ac2670b30752a346e
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-03-16 04:29:36 +0800

    added sign up interaction

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,3 +9,19 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.addEventListener("DOMContentLoaded",function () {
+    document.getElementById("sign-up-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const username = document.querySelector(".sign-up-form input[name='signup-username']").value;
+        const email = document.querySelector(".sign-up-form input[name='signup-email']").value;
+        const password = document.querySelector(".sign-up-form input[name='signup-password']").value;
+
+        sessionStorage.setItem("signup-username", username);
+        sessionStorage.setItem("signup-email", email);
+        sessionStorage.setItem("signup-password", password);
+
+        window.location.href = "./Assets/registered.html";
+    });
+});
